4904d050929f260260e4f4d2aeca139f8ccfebe4
Vladislav Kardashov <alcoloid@list.ru>
2024-01-16 23:38:35 +0300

    add solution 3

---
+++ module_2/lesson_5.2/solution_3.py
@@ -0,0 +1,12 @@
+old_price = "Art"
+new_price = "Desing"
+
+card = input("Введите карту товара: ")
+new_card = card.replace(old_price, new_price)
+
+print(new_card)
+
+#Задание 3: Замена ключевых слов
+#Описание:
+
+#Роман хочет заменить слово "Art" на "Design" в различных описаниях карточек товаров своего магазина.
