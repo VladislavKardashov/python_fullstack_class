66b033baab8a04d75b9a102df8da85ff5fa084f6
Vladislav Kardashov <alcoloid@list.ru>
2024-04-24 00:51:16 +0300

    finisht 3 file

---
+++ module_2/Lesson_8/solution_3.py
@@ -0,0 +1,16 @@
+products_list = {'Яблоко': 100, 'Банан': 80, 'Кофе': 250, 'Чай': 150}
+
+product_min = min(products_list, key=lambda x: products_list [x])
+product_max = max(products_list, key=lambda x: products_list [x])
+
+print(f"Самый дешевый товар: {product_list} - {product_list[product_min]} p.")
+print(f"Самый дорогой товар: {product_list} - {product_list[product_max]} p.")
+
+
+
+#Задача 3: Минимальная и максимальная цена
+#Описание:
+
+#Роман хочет определить самый дешевый и самый дорогой товар в магазине. 
+#Данные для словаря: {'Яблоко': 100, 'Банан': 80, 'Кофе': 250, 'Чай': 150}. 
+#Программа должна вывести название и цену самого дешевого и самого дорогого товара.
