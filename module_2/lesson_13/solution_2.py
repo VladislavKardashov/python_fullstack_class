c5897e956bf3f6c94cd1ba620666400e4cbb4dd4
Vladislav Kardashov <alcoloid@list.ru>
2024-06-10 00:15:34 +0300

    finished 2 solution

--- module_2/lesson_13/solution_2.py
+++ module_2/lesson_13/solution_2.py
@@ -1,4 +1,24 @@
+import time
 
+def timer(func): 
+    def wrapper(*args, **kwargs):    
+        start = time.time() # время начала функции
+        func(*args, **kwargs) # запуск функции
+        end = time.time() # время окончании функции
+        print(f"Execution {round(end-start, 2)} seconds")
+    return wrapper
+
+@timer
+def create_design(task, pause):
+    time.sleep(pause)
+    print(task)
+    
+
+task_1 = "Логотип Васильевский рынок"
+task_2 = "Макет сайта Логомашины"
+
+create_design(task_1, 3)
+create_design(task_2, 2)
 
 
 
