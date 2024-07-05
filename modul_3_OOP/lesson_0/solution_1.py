a0596e711200ef8120441964bf32cb0c78b2b4d9
Vladislav Kardashov <alcoloid@list.ru>
2024-07-05 22:10:39 +0300

    finished

--- modul_3_OOP/lesson_0/solution_1.py
+++ modul_3_OOP/lesson_0/solution_1.py
@@ -1,27 +1,38 @@
-from typing import Literal 
+from typing import Literal
+
 
 class Animal:
-    def __init__(  
-            self, 
+    def __init__(
+            self,
             animal_type: str,
+            feeds: Literal['травоядное', 'хищное'],
             animal_sound: str,
-            view: Literal["хищное", "травоядное"]
     ):
         self.animal_type = animal_type
+        self.feeds = feeds
         self.animal_sound = animal_sound
-        self.view = view
 
-    def __str__(self) -> str: 
-        return f'Это {self.view} животное {self.animal_type}!'
-    
+    def __str__(self) -> str:
+        return f'Это {self.feeds} животное {self.animal_type}!'
+
     def sound(self) -> str:
-        return f"{self.animal_type} издает следующие звуки {self.animal_sound}"
-    
-tiger = Animal("Тигр", "РрРрРр", "Хищное")
-zebra = Animal("Зебра", "Ква-Ха", "Травоятное")
+        return f'{self.animal_type} издает звуки {self.animal_sound}!'
+
+    def feed(self) -> str:
+        return f'{self.animal_type} кушает {self.feeds}!'
+
+
+tiger = Animal('тигр', 'хищное', 'ррр')
 
 print(tiger, tiger.sound())
+print(tiger, tiger.feed())
+
+zebra = Animal("зебра", 'травоядное', 'ква-ха')
+
 print(zebra, zebra.sound())
+print(zebra, zebra.feed())
+
+
 
 
 #Задание:
